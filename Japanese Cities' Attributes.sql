468d7f1ead47aa3f81307bd5e2d97504144d3a14
NIRMIT BANSAL <bansalnirmit3@gmail.com>
2026-02-17 19:13:20 +0530

    Add Japanese Cities' Attributes.sql

---
+++ Japanese Cities' Attributes.sql
@@ -0,0 +1,6 @@
+-- Problem: Japanese Cities' Attributes
+-- Platform: HackerRank
+-- Difficulty: Easy
+-- Link: https://www.hackerrank.com/challenges/japanese-cities-attributes/problem?isFullScreen=true
+
+select * from city where countrycode = 'JPN';
