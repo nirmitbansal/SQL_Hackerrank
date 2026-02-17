745c9f00623344b4548239963ea1d9885c6af3e2
NIRMIT BANSAL <bansalnirmit3@gmail.com>
2026-02-17 19:13:22 +0530

    Add Select By ID.sql

---
+++ Select By ID.sql
@@ -0,0 +1,6 @@
+-- Problem: Select By ID
+-- Platform: HackerRank
+-- Difficulty: Easy
+-- Link: https://www.hackerrank.com/challenges/select-by-id/problem?isFullScreen=true
+
+select id, name, countrycode, district, population from city where id = 1661;
