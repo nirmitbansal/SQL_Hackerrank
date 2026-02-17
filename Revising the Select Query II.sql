29b924f82cc429d45eb918b5bd7fc2a0e208af01
NIRMIT BANSAL <bansalnirmit3@gmail.com>
2026-02-17 19:13:21 +0530

    Add Revising the Select Query II.sql

---
+++ Revising the Select Query II.sql
@@ -0,0 +1,6 @@
+-- Problem: Revising the Select Query II
+-- Platform: HackerRank
+-- Difficulty: Easy
+-- Link: https://www.hackerrank.com/challenges/revising-the-select-query-2/problem?isFullScreen=true
+
+SELECT name from city where countrycode = 'USA' and population > 120000;
