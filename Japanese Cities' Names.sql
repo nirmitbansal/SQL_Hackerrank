6d1940340a62c6cef3d8e81039a1d6890a670f11
NIRMIT BANSAL <bansalnirmit3@gmail.com>
2026-02-17 19:13:21 +0530

    Add Japanese Cities' Names.sql

---
+++ Japanese Cities' Names.sql
@@ -0,0 +1,6 @@
+-- Problem: Japanese Cities' Names
+-- Platform: HackerRank
+-- Difficulty: Easy
+-- Link: https://www.hackerrank.com/challenges/japanese-cities-name/problem?isFullScreen=true
+
+select name from city where countrycode = 'JPN';
