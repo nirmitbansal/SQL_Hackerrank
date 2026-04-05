d64dabc23c42efb5773c81c8d7cc234106265db5
NIRMIT BANSAL <bansalnirmit3@gmail.com>
2026-04-05 21:00:13 +0530

    Add Weather Observation Station 6.sql

---
+++ Weather Observation Station 6.sql
@@ -0,0 +1,12 @@
+-- Problem: Weather Observation Station 6
+-- Platform: HackerRank
+-- Difficulty: Easy
+-- Link: https://www.hackerrank.com/challenges/weather-observation-station-6/problem?isFullScreen=true
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY LIKE 'A%'
+   OR CITY LIKE 'E%'
+   OR CITY LIKE 'I%'
+   OR CITY LIKE 'O%'
+   OR CITY LIKE 'U%';
