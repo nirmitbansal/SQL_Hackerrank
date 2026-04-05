a3f9db8053b0d0d813f8cfc96451af33896b043e
NIRMIT BANSAL <bansalnirmit3@gmail.com>
2026-04-05 21:00:15 +0530

    Add Weather Observation Station 9.sql

---
+++ Weather Observation Station 9.sql
@@ -0,0 +1,10 @@
+-- Problem: Weather Observation Station 9
+-- Platform: HackerRank
+-- Link: https://www.hackerrank.com/challenges/weather-observation-station-9/problem?isFullScreen=true
+
+SELECT DISTINCT CITY FROM STATION
+WHERE CITY NOT LIKE 'A%'
+AND CITY NOT LIKE 'E%'
+AND CITY NOT LIKE 'I%'
+AND CITY NOT LIKE 'O%'
+AND CITY NOT LIKE 'U%';
