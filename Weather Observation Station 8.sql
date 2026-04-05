3ed4d54e9eda0f96df5cc28d39e8d3275648b423
NIRMIT BANSAL <bansalnirmit3@gmail.com>
2026-04-05 21:00:14 +0530

    Add Weather Observation Station 8.sql

---
+++ Weather Observation Station 8.sql
@@ -0,0 +1,10 @@
+-- Problem: Weather Observation Station 8
+-- Platform: HackerRank
+-- Link: https://www.hackerrank.com/challenges/weather-observation-station-8/problem?isFullScreen=true
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE 
+    (CITY LIKE 'A%' OR CITY LIKE 'E%' OR CITY LIKE 'I%' OR CITY LIKE 'O%' OR CITY LIKE 'U%')
+AND
+    (CITY LIKE '%a' OR CITY LIKE '%e' OR CITY LIKE '%i' OR CITY LIKE '%o' OR CITY LIKE '%u');
