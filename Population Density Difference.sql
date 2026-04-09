cfa9d76a8876b0b90fdd54df4b50840977f81e6c
NIRMIT BANSAL <bansalnirmit3@gmail.com>
2026-04-09 21:00:08 +0530

    Add Population Density Difference.sql

---
+++ Population Density Difference.sql
@@ -0,0 +1,5 @@
+-- Problem: Population Density Difference
+-- Platform: HackerRank
+-- Link: https://www.hackerrank.com/challenges/population-density-difference/problem?isFullScreen=true
+
+SELECT max(population) - min(population) from city;
